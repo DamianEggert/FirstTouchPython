d54379637897028cbfd09400df2c8c7e9ae05275
Damian Eggert <damian.eggert@wp.pl>
2020-02-21 12:59:51 +0100

    Initial commit

---
+++ all.py
@@ -0,0 +1,51 @@
+from math import *
+"""
+print("Hello")
+
+name = "Damian"
+age = 22
+
+#obe zmienne musza byc stringiem aby moc je dodac
+print(name + str(age))
+
+monitor = input("Wprowadz nazwe monitora: \n")
+print(monitor)
+print(len(monitor))
+print(monitor[0])
+
+mysz = input("Wprowadz dpi myszki: ")
+print(int(mysz))
+
+num1 = input("Wprowadz liczbe: ")
+num2 = input("Wprowadz nastepna liczbe: ")
+print(float(num1) + float(num2))
+print(pow(int(num1), int(num2)))
+print(max(int(num1), int(num2)))
+print(min(int(num1), int(num2)))
+result = sqrt(int(num1))
+print(result)
+
+list = ["Damian", 23, True, "Agata", 21, False] #pojebane xd
+print(list[2])
+print(list[2:])
+list[2] = False
+print(list[2:5])
+print(list)
+
+data = ["Krystian", "Piotr", 18, 23, True, True]
+data.extend(list)
+print(data)
+data.append("Nowy")
+print(data)
+data.insert(1,"Huehue")
+print(data)
+data.remove(18)
+print(data)
+print(data.index(23))
+
+dataData = data.copy()
+data.clear()
+print(dataData)
+print(data)
+"""
+
