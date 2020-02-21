f1a108704e93f3cfdd49ecbeded85a4d2dbc3223
Damian Eggert <damian.eggert@wp.pl>
2020-02-22 00:24:49 +0100

    add first function

--- all.py
+++ all.py
@@ -49,3 +49,13 @@ print(dataData)
 print(data)
 """
 
+
+def credential():
+    name = input("What is your name?\n")
+    surname = input("What is your surname?\n")
+    age = input("What is your age?\n")
+    sex = input("What is your sex?\n")
+    print("Hello " + name + surname + ". You are " + str(age) + " years old " + sex + ".")
+
+
+credential()
