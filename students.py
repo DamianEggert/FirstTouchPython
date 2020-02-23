c39894a7ed572e008cb47d9f26b7c0401949a6c3
Damian Eggert <damian.eggert@wp.pl>
2020-02-23 14:21:33 +0100

    add spaces

--- students.py
+++ students.py
@@ -5,3 +5,5 @@ student2 = Student("Agata", "Dentis", 5.1, False)
 
 print("Is " + student1.name + " honor to upper class?\n" + str(student1.on_honor_roll()))
 print("Is " + student2.name + " honor to upper class?\n" + str(student2.on_honor_roll()))
+
+
